0bc0ba722da6c22b747887eace3fa774c7a34df7
mishrabhavesh <mishra.bhavesh@outlook.com>
2020-03-21 14:21:43 +0530

    add edit update show and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+  @article = Article.all  
+  end
+
   def new
     @article = Article.new
   end
@@ -11,13 +15,26 @@ class ArticlesController < ApplicationController
     else
       render :new
     
-    end`
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def show
     @article =Article.find(params[:id])
   end
 
+  def update
+  @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was seccessfully updated "
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
 
   def article_params
     params.require(:article).permit(:title, :description)
