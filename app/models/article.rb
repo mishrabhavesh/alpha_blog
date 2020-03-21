d2f647baae5958bb43d34380232afa439ca34f5a
mishrabhavesh <mishra.bhavesh@outlook.com>
2020-03-22 03:15:51 +0530

    create menu fixed

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
 
 validates :title , presence: true , length: { minimum: 5 , maximum: 50 }
 validates_presence_of :description
-validates :description , length: { minimum: 10 , maximum: 150 }
+validates :description , length: { minimum: 10 , maximum: 1000 }
 end
