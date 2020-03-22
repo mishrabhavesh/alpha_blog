0dd66f70538ec81582575a3a05b8978e1acf6fb5
mishrabhavesh <mishra.bhavesh@outlook.com>
2020-03-22 17:56:05 +0530

    added password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
                length: {maximum: 50}, format: { with: VALID_EMAIL_REGEX }
     has_many :articles
     before_save {self.email = email.downcase}
+    has_secure_password
 end
